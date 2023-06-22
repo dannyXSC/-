1159adadf8f30c44d95c5b1f688d741283249c3d
dannyXSC <874794016@qq.com>
2023-06-22 22:09:47 +0800

    修改格式

--- code/main.py
+++ code/main.py
@@ -3,11 +3,10 @@ import requests
 from selenium import webdriver
 from Inventory import get_info
 
-# driver_path = "D:\\web_driver\\chromedriver\\chromedriver.exe"
 from Search import search
 
+# driver_path = "D:\\web_driver\\chromedriver\\chromedriver.exe"
 driver_path = "/Users/xiesicheng/Desktop/OpenSource/chromedriver/chromedriver"
-database_url = "https://patentscope.wipo.int/search/en/result.jsf?query=IC:%22F02C%203/28%22"
 
 driver = webdriver.Chrome(executable_path=driver_path)
 try:
